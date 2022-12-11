51035c070ca026f3231a454705bb0d6934cbd818
numbafye <saadiqgp@gmail.com>
2022-12-11 14:10:45 -0500

    .map() put mockdata stocks into index.js chart

--- public/index.js
+++ public/index.js
@@ -13,17 +13,19 @@ async function main() {
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor:  'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)'
-            }]
+            }))
         }
     });
     
-
 }
 
 main()
+
+                                                 
+                                     
